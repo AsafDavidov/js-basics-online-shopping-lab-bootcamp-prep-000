a182d7fb417c1dcfadd36dec74f0b5507b0aa5ae
Asaf Davidov <adavidov@conncoll.edu>
2018-05-07 21:30:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function removeFromCart(item) {
   var removedItem = false;
   for (var i = 0; i<cart.length;i++){
     if (cart[i].itemName == item){
-      cart.splice(i,0);
+      cart.splice(i,1);
       removedItem = true;
       return "cheese";
     }
